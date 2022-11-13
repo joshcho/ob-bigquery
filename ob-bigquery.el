68594dce1896ba3a608cf290d6f2ff75bbbd073d
Josh Cho <joshchonpc@gmail.com>
2022-11-13 13:56:55 -0800

    Refactor ob-bigquery-ctrl-c-ctrl-c

--- ob-bigquery.el
+++ ob-bigquery.el
@@ -1,16 +1,8 @@
 (require 'ob-python)
-;; follow instructions on https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries to set up bigquery for python
 
-;; run this function within org babel code block with sql (it will replace sql and run associated python). note that this calls list on the output, so make sure to add LIMIT to your queries.
-(defun org-babel-bigquery-ctrl-c-ctrl-c ()
-  (interactive)
-  (let ((saved-pos (point)))
-    (search-backward-regexp "#\\+begin_src sql.*\C-j")
-    (let* ((begin-pos (match-end 0))
-           (end-pos (save-excursion (re-search-forward "#\\+end_src")
-                                    (- (match-beginning 0) 1)))
-           (saved-block (buffer-substring-no-properties begin-pos end-pos))
-           (fmt-string "from google.cloud import bigquery
+;; hard cap for display
+(defvar ob-max-num-rows 30)
+(defvar ob-fmt-string "from google.cloud import bigquery
 client = bigquery.Client()
 job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
 query_job = client.query(
@@ -18,24 +10,38 @@ query_job = client.query(
 %s
 \"\"\"
 )
-results = list(query_job.result())
-return [list(results[0].keys())] + list(map(lambda row: list(row.values()), results))"))
-      (goto-char begin-pos)
-      (delete-char (- end-pos begin-pos))
-      (insert (format fmt-string saved-block))
-      (cl-labels ((replace-lang (from to)
-                    (progn
-                      (search-backward-regexp (concat "#\\+begin_src " from))
-                      (goto-char (match-end 0))
-                      (backward-delete-word 1)
-                      (insert to))))
-        (replace-lang "sql" "python")
-        (call-interactively 'org-ctrl-c-ctrl-c)
-        (replace-lang "python" "sql"))
-      (goto-char begin-pos)
-      (let ((new-end-pos (save-excursion (re-search-forward "#\\+end_src")
-                                         (- (match-beginning 0) 1))))
-        (delete-char (- new-end-pos begin-pos)))
-      (insert saved-block)
-      (goto-char saved-pos))))
-(define-key org-mode-map (kbd "C-c C-e") #'org-babel-bigquery-ctrl-c-ctrl-c)
+results = []
+for res in query_job.result():
+    if len(results) > %s:
+        break
+    results += [res]
+return [list(results[0].keys())] + list(map(lambda row: list(map(lambda val: val if val else \"null\", row.values())), results))")
+
+(defun ob-bigquery-ctrl-c-ctrl-c ()
+  (interactive)
+  (let* ((saved-pos (point))
+         (begin-pos (re-search-backward "#\\+begin_src \\(sql\\).*\C-j"))
+         (lang-pos (match-beginning 1))
+         (begin-source-pos (match-end 0))
+         (end-pos (re-search-forward "\C-j#\\+end_src"))
+         (end-source-pos (match-beginning 0))
+         (saved-block (buffer-substring-no-properties begin-source-pos end-source-pos)))
+    ;; replace sql block with python block
+    (goto-char begin-source-pos)
+    (delete-char (- end-source-pos begin-source-pos))
+    (insert (format ob-fmt-string saved-block ob-max-num-rows))
+    ;; swap in python for lang, run, then swap sql back in
+    (goto-char lang-pos)
+    (delete-word 1)
+    (insert "python")
+    (call-interactively 'org-ctrl-c-ctrl-c)
+    (goto-char lang-pos)
+    (delete-word 1)
+    (insert "sql")
+    ;; replace python block with sql block
+    (goto-char begin-source-pos)
+    (delete-char (- (save-excursion (re-search-forward "\C-j#\\+end_src")
+                                    (match-beginning 0))
+                    begin-source-pos))
+    (insert saved-block)
+    (goto-char saved-pos)))
