c3988a8731e6a1616dd34c115f4034797f9e0b8c
Josh Cho <joshchonpc@gmail.com>
2022-11-13 15:33:56 -0800

    Add :store keyword support

--- ob-bigquery.el
+++ ob-bigquery.el
@@ -1,7 +1,6 @@
 (require 'ob-python)
 
-;; hard cap for display
-(defvar ob-max-num-rows 30)
+(defvar ob-num-max-rows 25)
 (defvar ob-fmt-string "from google.cloud import bigquery
 client = bigquery.Client()
 job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
@@ -10,12 +9,9 @@ query_job = client.query(
 %s
 \"\"\"
 )
-results = []
-for res in query_job.result():
-    if len(results) > %s:
-        break
-    results += [res]
-return [list(results[0].keys())] + list(map(lambda row: list(map(lambda val: val if val else \"null\", row.values())), results))")
+results = list(query_job.result())
+%s = list(map(lambda row: list(map(lambda val: val if val else \"null\", row.values())), results))
+%s[list(results[0].keys())] + %s[:%s]")
 
 (defun ob-bigquery-ctrl-c-ctrl-c ()
   (interactive)
@@ -23,25 +19,44 @@ return [list(results[0].keys())] + list(map(lambda row: list(map(lambda val: val
          (begin-pos (re-search-backward "#\\+begin_src \\(sql\\).*\C-j"))
          (lang-pos (match-beginning 1))
          (begin-source-pos (match-end 0))
+         (store-var-p (save-excursion (re-search-forward ":store \\([^\s\C-j]*\\)[\s\C-j]"
+                                                         (save-excursion (end-of-line)
+                                                                         (+ 1 (point)))
+                                                         t)))
+         (store-var (save-excursion (if (re-search-forward ":store \\([^\s\C-j]*\\)[\s\C-j]"
+                                                           (save-excursion (end-of-line)
+                                                                           (+ 1 (point)))
+                                                           t)
+                                        (substring-no-properties (match-string 1))
+                                      "table")))
          (end-pos (re-search-forward "\C-j#\\+end_src"))
          (end-source-pos (match-beginning 0))
-         (saved-block (buffer-substring-no-properties begin-source-pos end-source-pos)))
+         (saved-source-block (buffer-substring-no-properties begin-source-pos end-source-pos))
+         (saved-block (buffer-substring-no-properties begin-pos end-pos)))
     ;; replace sql block with python block
     (goto-char begin-source-pos)
     (delete-char (- end-source-pos begin-source-pos))
-    (insert (format ob-fmt-string saved-block ob-max-num-rows))
+    (insert (format ob-fmt-string saved-source-block store-var
+                    (if store-var-p ""
+                      "return ")
+                    store-var
+                    ob-num-max-rows))
     ;; swap in python for lang, run, then swap sql back in
     (goto-char lang-pos)
     (delete-word 1)
     (insert "python")
+    (when store-var-p
+      (insert " :session "))
     (call-interactively 'org-ctrl-c-ctrl-c)
     (goto-char lang-pos)
     (delete-word 1)
     (insert "sql")
+    (when store-var-p
+      (delete-char (length " :session ")))
     ;; replace python block with sql block
-    (goto-char begin-source-pos)
-    (delete-char (- (save-excursion (re-search-forward "\C-j#\\+end_src")
-                                    (match-beginning 0))
-                    begin-source-pos))
+    (goto-char begin-pos)
+    (delete-char (- (save-excursion (re-search-forward "\C-j#\\+end_src"))
+                    begin-pos))
     (insert saved-block)
-    (goto-char saved-pos)))
+    (goto-char saved-pos)
+    ))
